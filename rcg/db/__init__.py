75310fbfc6140d5b7cf15392f3aa3d13508c85c3
Andy Friedman <afriedman412@gmail.com>
2022-12-05 12:57:28 -0500

    first upload for sked only repo

--- rcg/db/__init__.py
+++ rcg/db/__init__.py
@@ -2,22 +2,13 @@
 from pymysql import connect
 import os
 
-if os.environ.get('FLASK_ENV') == "development":
-    mysql_conn = connect(
-        user=os.environ.get('LOCAL_MYSQL_USER'),
-        password=os.environ.get('LOCAL_MYSQL_PW'),
-        host=os.environ.get('LOCAL_MYSQL_URL'),
-        port=3306,
-        db='rcg-testo'
-    )
 
-else:
-    mysql_conn = connect(
-        user=os.environ.get('MYSQL_USER'),
-        password=os.environ.get('MYSQL_PW'),
-        host=os.environ.get('MYSQL_URL'),
-        port=3306,
-        db='rcg'
+mysql_conn = connect(
+    user=os.environ.get('MYSQL_USER'),
+    password=os.environ.get('MYSQL_PW'),
+    host=os.environ.get('MYSQL_URL'),
+    port=3306,
+    db='rcg'
     )
 
 def db_commit(q, conn=mysql_conn):
